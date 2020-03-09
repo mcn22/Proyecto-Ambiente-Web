993db90f3a3d0584a0d00844f434012ef2bd3e8f
A1DS19 <59151555+A1DS19@users.noreply.github.com>
2020-03-08 19:37:47 -0600

    updates a UI y demas

--- index.php
+++ index.php
@@ -1,24 +1,53 @@
-<?php require_once 'includes/header.php';?>
-    <div class="container">
-    <section id="seccion" >
-        <div class="post">
-            <h1>Un post</h1>
-            <img src="assets/img/bh.jpg" alt="blackHole" class="img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Eu scelerisque felis imperdiet 
-                proin. Est ante in nibh mauris cursus mattis molestie a. Varius duis at consectetur
-                lorem donec. Tincidunt vitae semper quis lectus. Elementum pulvinar etiam
-                non quam lacus suspendisse faucibus interdum posuere. Lacus laoreet non 
-                curabitur gravida arcu ac tortor dignissim convallis. Congue eu consequat 
-                ac felis donec et. Venenatis cras sed felis eget velit aliquet sagittis id.
-                Dictum fusce ut placerat orci. Amet tellus cras adipiscing enim eu turpis 
-                egestas pretium aenean. Nunc id cursus metus aliquam eleifend mi in nulla.
-                Diam maecenas ultricies mi eget mauris. Nec feugiat nisl pretium fusce.</p>
+<?php require_once 'includes/header.php'; ?>
+
+<section class="container">
+    <h2 id="h2-nosotros">Sobre Nosotros</h2>
+
+    <div class="informacion-nosotros">
+        <div class="icono">
+            <img src="../Proyecto-Ambiente-Web/assets/img/bh.jpg" alt="ICONO SEGURIDAD">
         </div>
-    </section>
-    <?php require_once 'includes/sidebar.php';?>
+        <h3>Seguridad</h3>
+        <p>Sed posuere vulputate lectus, sit amet volutpat sem ultrices vitae. Nullam et ex at lorem pretium mattis. Cras hendrerit elit eu tempor cursus. Aenean id ultricies massa. Phasellus rhoncus nulla malesuada rutrum tincidunt. Sed scelerisque ultricies semper. Proin.</p>
     </div>
-    <?php require_once 'includes/footer.php';?>
+
+    <div class="informacion-nosotros">
+        <div class="icono">
+            <img src="../Proyecto-Ambiente-Web/assets/img/bh.jpg" alt="ICONO TIEMPO">
+        </div>
+        <h3>Entregas a Tiempo</h3>
+        <p>Sed posuere vulputate lectus, sit amet volutpat sem ultrices vitae. Nullam et ex at lorem pretium mattis. Cras hendrerit elit eu tempor cursus. Aenean id ultricies massa. Phasellus rhoncus nulla malesuada rutrum tincidunt. Sed scelerisque ultricies semper. Proin.</p>
+    </div>
+
+    <div class="informacion-nosotros">
+        <div class="icono">
+            <img src="../Proyecto-Ambiente-Web/assets/img/bh.jpg" alt="ICONO MEJOR PRECIO">
+        </div>
+        <h3>Los Mejores Precios</h3>
+        <p>Sed posuere vulputate lectus, sit amet volutpat sem ultrices vitae. Nullam et ex at lorem pretium mattis. Cras hendrerit elit eu tempor cursus. Aenean id ultricies massa. Phasellus rhoncus nulla malesuada rutrum tincidunt. Sed scelerisque ultricies semper. Proin.</p>
+    </div>
+</section>
+
+<main class="container">
+    <h2>Venta</h2>
+
+
+    <div class="producto">
+        <div class="img-producto">
+            <img src="../Proyecto-Ambiente-Web/assets/img/bh.jpg" alt="IMAGEN PRODUCTO">
+        </div>
+        <div class="contenido-producto">
+            <h3>NOMBRE PRODUCTO</h3>
+            <p>DESCRIPCCION PRODUCTO</p>
+            <p>PRECIO PRODUCTO</p>
+            <a href="#">VER PRODUCTO</a>
+        </div>
+    </div>
+
+
+    
+</main>
+<?php require_once 'includes/footer.php'; ?>
 </body>
 
 </html>
