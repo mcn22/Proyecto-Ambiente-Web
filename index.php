120c3dcce02cd6adc9ed8d9534ea6f4e6c7ef7d7
A1DS19 <jp06171@gmail.com>
2020-03-12 21:41:42 -0600

    Productos de db en index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php require_once 'includes/header.php'; ?>
-<?php require_once 'includes/conexion.php'; ?>
 
 <section class="container">
     <h2 id="h2-nosotros">Sobre Nosotros</h2>
@@ -29,21 +28,35 @@
     </div>
 </section>
 
+<?php 
+require_once 'includes/conexion.php'; 
+    $select = "SELECT * FROM `producto` ";
+    $query = mysqli_query($db, $select);
+    $imagen = "";
+    while ($row = mysqli_fetch_array($query)) {
+        $imagen = $row['imageName'];
+        $nombre = $row['nombre'];
+        $desc = $row['descripccion'];
+        $precio = $row['precio']; ?>
 
-<main class="container">
+        <main class="container">
     <div class="producto">
         <div class="img-producto">
-            <img src="../Proyecto-Ambiente-Web/assets/img/bh.jpg" alt="IMAGEN PRODUCTO">
+            <?php echo '<img src="imagenesProductos/'.$imagen.'">';?>
         </div>
         <div class="contenido-producto">
-            <h3>NOMBRE PRODUCTO</h3>
-            <p>DESCRIPCCION PRODUCTO</p>
-            <p>PRECIO PRODUCTO</p>
+            <h3><?php echo $nombre ?></h3>
+            <p><?php echo $desc ?></p>
+            <p>$ <?php echo $precio ?></p>
             <a href="#">VER PRODUCTO</a>
         </div>
     </div>
 
-
+   <?php }
+    if (mysqli_close($db)) {
+        
+        }
+    ?>
 
 </main>
 <?php require_once 'includes/footer.php'; ?>
